6fbee5c354ce9af2fb1e6d0115d5bcf464e2426a
Hugo2611 <118531843+Hugo2611@users.noreply.github.com>
2024-05-25 19:50:03 +0200

    Delete AbstractFactory directory

--- AbstractFactory/GraphicComponentFactory.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactory;
-
-import javax.swing.JButton;
-import javax.swing.JMenu;
-
-public interface GraphicComponentFactory {
-    JButton createButton(String text);
-    JMenu createMenu(String text);
-}
